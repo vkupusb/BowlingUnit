3deb9193c775a8e182b03468bfb3675a7de1c792
vkupusb <kvishva27@gmail.com>
2025-03-24 18:04:08 +0530

    updated comments and readme file

--- src/BowlingUnit.cpp
+++ src/BowlingUnit.cpp
@@ -1,6 +1,11 @@
 #include "BowlingUnit.h"
 #include <algorithm>
 
+/*
+BowlingGame~COD~Trace
+@VRF_SRS: NA
+@VRF_SDD: NA
+*/
 void BowlingGame::Unit::BowlingUnit::addPlayer(std::string_view first_name,std::string_view last_name)
 {
     auto player{ std::make_shared<BowlingGame::Players::Player>() };
@@ -9,11 +14,21 @@ void BowlingGame::Unit::BowlingUnit::addPlayer(std::string_view first_name,std::
 
 }
 
+/*
+BowlingGame~COD~Trace
+@VRF_SRS: NA
+@VRF_SDD: NA
+*/
 void BowlingGame::Unit::BowlingUnit::removePlayer(std::string_view first_name,std::string_view last_name)
 {
-   
+   //will be implemented later
 }
 
+/*
+BowlingGame~COD~Trace
+@VRF_SRS: NA
+@VRF_SDD: NA
+*/
 void BowlingGame::Unit::BowlingUnit::startGame()
 {
     std::for_each(players.begin(),players.end(),[](auto player){
