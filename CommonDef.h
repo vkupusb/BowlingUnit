c9b47af992c0faf71f580cffafa29a6ca54d2959
Vishvanathan <75823279+vkupusb@users.noreply.github.com>
2025-03-24 15:39:55 +0530

    Delete CommonDef.h

--- CommonDef.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <cstdint>
-constexpr unsigned int MIN_ROLL_SCORE{ 0 };
-constexpr unsigned int MAX_ROLL_SCORE{ 10 };
-constexpr unsigned int MAX_TOTAL_SCORE{ 300 };
-constexpr unsigned int MAX_FRAME_COUNT{ 10 };
